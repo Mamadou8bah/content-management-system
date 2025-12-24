acfa2bbaa99288efc82b552fbbbcbf3408013704
mamadou8bah <mbah18791@gmail.com>
2025-12-24 07:21:57 +0000

    Changed database

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,7 +1,12 @@
 const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+dotenv.config();
 
 function connectToDb(){
-    return mongoose.connect('mongodb://localhost:27017/cmsdb')
+    return mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
         .then(() => {
             console.log('Succesfully connected to database');
         })

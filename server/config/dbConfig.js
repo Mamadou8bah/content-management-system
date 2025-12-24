d09263072c9c2264d6365350df8f4495bd675260
mamadou8bah <mbah18791@gmail.com>
2025-12-24 07:43:07 +0000

    Modofied DB config

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -3,10 +3,7 @@ const dotenv=require('dotenv');
 dotenv.config();
 
 function connectToDb(){
-    return mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    return mongoose.connect(process.env.MONGO_URI)
         .then(() => {
             console.log('Succesfully connected to database');
         })
